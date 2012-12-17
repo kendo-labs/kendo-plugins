c51564dee05f58a91a35fead779df9012e31775e
Burke Holland <burkeholland@gmail.com>
2012-12-17 17:19:27 -0600

    Wrong Name

--- github-pages/js/app.js
+++
@@ -1,6 +0,0 @@
-angular.module('plugins', []).
-  config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-      when('/maps', { templateUrl: 'partials/maps/index.html', controller: MapsCtrl }).
-      otherwise({redirectTo: '/'});
-}]);
