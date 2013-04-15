da2a7a79b362a2b685edec632e0971d86bc2e553
Zack <amaury.richard@gmail.com>
2013-04-15 18:07:14 +0300

    Update kendo.repeater.js
    Uses AMD or browser globals to create a jQuery plugin

--- Web/Simple Plugin Examples/DataSource/kendo.repeater.js
+++ Web/Simple Plugin Examples/DataSource/kendo.repeater.js
@@ -1,4 +1,17 @@
-(function() {
+// Uses AMD or browser globals to create a jQuery plugin.
+
+// It does not try to register in a CommonJS environment since
+// jQuery is not likely to run in those environments.
+// See jqueryPluginCommonJs.js for that version.
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery', 'kendo'], factory);
+    } else {
+        // Browser globals
+        factory(window.jQuery, window.kendo);
+    }
+}(function() {
     var kendo = window.kendo,
         ui = kendo.ui,
         Widget = ui.Widget,
